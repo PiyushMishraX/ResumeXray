4397fb79f8bd3f9a0533b2f2e6e5c4acd51e0157
Piyushmishrax <piyushmishra.connect@gmail.com>
2026-05-29 16:19:57 +0530

    Resolve the dns issue

--- Backend/server.js
+++ Backend/server.js
@@ -2,8 +2,8 @@ require("dotenv").config()
 const app = require("./src/app")
 const connectToDB = require("./src/config/database")
 
-// const dns = require("dns")
-// dns.setServers(["1.1.1.1","8.8.8.8"]);
+const dns = require("dns")
+dns.setServers(["1.1.1.1","8.8.8.8"]);
 
 
 connectToDB()
