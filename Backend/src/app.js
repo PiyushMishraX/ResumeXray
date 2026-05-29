59075a7c2d1a66c6f1275c0ac786565da62f8db9
Piyushmishrax <piyushmishra.connect@gmail.com>
2026-05-29 16:39:48 +0530

    Connect auth routes to app js for the auth api creation

--- Backend/src/app.js
+++ Backend/src/app.js
@@ -5,5 +5,14 @@ const app = express();
 
 app.use(express.json()) // middleware allowing us to read the data coming on request.body
 
+/* require all the routes here */
+const authRouter = require("./routes/auth.routes")
+
+// /api.auth is prefix to access all auth related apis
+// app.use("/api/auth", authRouter)
+
+/* using all the routes here */
+app.use("/api/auth", authRouter)
+
 
 module.exports = app;
--- Backend/src/routes/auth.routes.js
+++ Backend/src/routes/auth.routes.js
@@ -1,1 +1,19 @@
 // file to create the auth realted apis
+
+// const { Router } = reuqire('express')
+// const authRouter = Router() // using destructured Router instead of express.Router
+
+const express = reuqire('express')
+
+const authRouter = express.Router()
+
+// it is js comment string or JSDoc comments
+
+/**
+ * @route POST
+ * @description Register a new user
+ * @access Public
+ */
+authRouter.post("/register", )
+
+module.exports = authRouter;
