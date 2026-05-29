cac43b4ded3c6804ac15268c717bdceac1aa195a
Piyushmishrax <piyushmishra.connect@gmail.com>
2026-05-29 16:19:40 +0530

    Connect with the Database

--- Backend/server.js
+++ Backend/server.js
@@ -1,8 +1,12 @@
 require("dotenv").config()
 const app = require("./src/app")
+const connectToDB = require("./src/config/database")
 
 // const dns = require("dns")
 // dns.setServers(["1.1.1.1","8.8.8.8"]);
+
+
+connectToDB()
 
 
 app.listen(3000, ()=>{
