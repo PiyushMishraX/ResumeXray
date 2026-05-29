c57d2d9f181b8cf82618c123033aab207634cb28
Piyushmishrax <piyushmishra.connect@gmail.com>
2026-05-29 17:04:54 +0530

    Define registration failre conditions

--- Backend/src/controllers/auth.controller.js
+++ Backend/src/controllers/auth.controller.js
@@ -7,7 +7,36 @@ const userModel = require("../models/user.model")
  */
 async function registerUserController( req, res){
 
+    const { username , email , password} =  req.body
 
+    if(!username || !email || !password) {
+        return res.status(400).json({
+            message: "Please provide username, email and password",
+        })
+    }
+ 
+    const ifUserAlreadyExists = await userModel.findOne({
+        // $or can have 1 or more conditions in the array each object inside is a condition
+        // the $or returns that after any of the condition is met 
+        $or: [ {username} , {email}]
+    })
+
+    if(ifUserAlreadyExists) {
+        /* isUserAlreadyExists.username == username <-- better message condition */
+        // return res.status(400).json({
+        //     message: "Account already exists with this email address or username",
+        // })
+
+        if(isUserAlreadyExists.username == username){
+            return res.status(400).json({
+                message: "Account already exists with this username",
+            })
+        } else {
+            return res.status(400).json({
+                message: "Account already exists with this email address",
+            })
+        }
+    }
 }
 
 
