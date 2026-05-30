aec87b860f7f58c1ee311a7ac215d73fc52993bb
Piyushmishrax <piyushmishra.connect@gmail.com>
2026-05-30 22:42:11 +0530

    Create login form

--- Frontend/src/features/auth/pages/Login.jsx
+++ Frontend/src/features/auth/pages/Login.jsx
@@ -6,7 +6,29 @@ import React from 'react'
 
 const Login = () => {
   return (
-    <div>Login</div>
+    <main> {/* every page iside the main block */}
+    <div className="form-container">
+        <h1>Login</h1>
+
+        <form > 
+            {/* actions written from js */}
+
+            <div className="input-group">
+                <label htmlFor='email'>Email</label>
+                <input type="email" id='email' name='email' placeholder='Enter email address'/>
+            </div>
+            <div className="input-group">
+                <label htmlFor='password'>Email</label>
+                <input type="password" id='password' name='password' placeholder='Enter password'/>
+            </div>
+
+            <button className='button primary-button'>Login </button>
+
+        </form>
+    </div>
+
+
+    </main>
   )
 }
 
