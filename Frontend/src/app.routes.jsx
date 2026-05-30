7b37296efc3e2541f602e4a7f7043b6a8d018b33
Piyushmishrax <piyushmishra.connect@gmail.com>
2026-05-30 21:19:28 +0530

    Initalize sassy css file and use browser router from react router

---
+++ Frontend/src/app.routes.jsx
@@ -0,0 +1,6 @@
+import { createBrowserRouter } from "react-router" // react-router-dom now changed to react-router 
+
+//  creating dommy pages not exact ui
+export const router = createBrowserRouter([
+
+])
