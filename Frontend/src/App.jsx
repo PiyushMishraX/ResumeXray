754b7c3f60f0dad89994e0546c8d2a5ef3254e71
Piyushmishrax <piyushmishra.connect@gmail.com>
2026-05-30 21:52:54 +0530

    Export route in the core file

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,11 +1,13 @@
-
-import React from 'react'
+// import React from 'react'
+import { RouterProvider } from 'react-router'
+import { router } from "./app.routes.jsx" // do not dforget to add .jsx extension else will give error , probably because of small letter app
 
 
 const App = () => {
   return (
-      <>
-      </>
+     <>
+      <RouterProvider router={router}/>
+     </>
   )
 }
 
--- Frontend/src/app.routes.jsx
+++ Frontend/src/app.routes.jsx
@@ -1,16 +1,25 @@
-import { createBrowserRouter } from "react-router" // react-router-dom now changed to react-router 
+import { createBrowserRouter} from "react-router" // react-router-dom now changed to react-router 
 import Login from "./features/auth/pages/Login"
 import Register from "./features/auth/pages/Register"
+import App from "./App"
+
+import {Navigate } from "react-router"
 
 //  creating dommy pages not exact ui
 //  router creater with two routes
 export const router = createBrowserRouter([
     {
+        path: "/", // for now set "/" route to login
+        element: <Navigate to="/login" replace />
+
+    },
+    {
         path: "/login",
-        element:<Login/>
+        element:<Login />
     },
     {
         path: "/register",
-        element:<Login/>
+        element:<Register />
     },
 ])
+
