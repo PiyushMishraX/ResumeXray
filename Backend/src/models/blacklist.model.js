6d432a63a5ca0b4ecaf63548f2d4771d761b79ff
Piyushmishrax <piyushmishra.connect@gmail.com>
2026-05-30 19:10:11 +0530

    Set blacklist model document after 1 day( token expiration time

--- Backend/src/models/blacklist.model.js
+++ Backend/src/models/blacklist.model.js
@@ -9,6 +9,13 @@ const blackListTokenSchema = new mongoose.Schema({
     timestamps: true // databse tracks when the token is balcklisted ( or added in the balck list model)
 })
 
+
+// createdAt is the first index the mongo db runs a background thread every 60 seconds the index at 1 get checked ( 1 index means the value added first) then if it's time have ended (1 day ) then it get deleted
+// after 1 day our jwt token itself expires and can not be used so deleting it from blacklist model will free up space
+blackListTokenSchema.index({ createdAt:1}, {
+    expireAfterSeconds: 60* 60* 24* 1, // expire in one day
+})
+
 const tokenBlacklistModel = mongoose.model("blacklistTokens", blackListTokenSchema)
 
 module.exports = tokenBlacklistModel
