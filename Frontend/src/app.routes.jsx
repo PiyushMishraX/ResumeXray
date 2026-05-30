c28c36a55559d14f23a12935fd35d838fd0dbd4f
Piyushmishrax <piyushmishra.connect@gmail.com>
2026-05-30 21:26:04 +0530

    Create router with two routes /login and /register

--- Frontend/src/app.routes.jsx
+++ Frontend/src/app.routes.jsx
@@ -1,6 +1,16 @@
 import { createBrowserRouter } from "react-router" // react-router-dom now changed to react-router 
+import Login from "./features/auth/pages/Login"
+import Register from "./features/auth/pages/Register"
 
 //  creating dommy pages not exact ui
+//  router creater with two routes
 export const router = createBrowserRouter([
-
+    {
+        path: "/login",
+        element:<Login/>
+    },
+    {
+        path: "/register",
+        element:<Login/>
+    },
 ])
---
+++ Frontend/src/features/auth/pages/Login.jsx
@@ -0,0 +1,13 @@
+// full code of login page
+// and components folder the reapeating elements are present such as input etc
+
+
+import React from 'react'
+
+const Login = () => {
+  return (
+    <div>Login</div>
+  )
+}
+
+export default Login
