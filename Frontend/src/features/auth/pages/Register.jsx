4e7e072224173539d3dc29f48f246c36ce708aff
Piyushmishrax <piyushmishra.connect@gmail.com>
2026-05-31 23:34:41 +0530

    Create navigation form

--- Frontend/src/features/auth/pages/Register.jsx
+++ Frontend/src/features/auth/pages/Register.jsx
@@ -1,8 +1,38 @@
 import React from 'react'
 
 const Register = () => {
+
+  const handleSubmit = (e) =>{
+        e.preventDefault() // stop the reload
+    }
+
   return (
-    <div>Register</div>
+    <main>
+    <div className="form-container">
+
+        <form onSubmit={handleSubmit} > 
+
+            <div className="input-group">
+                <label htmlFor='username'>Username</label>
+                <input type="text" id='username' name='email' placeholder='Enter username'/>
+            </div>
+            <div className="input-group">
+                <label htmlFor='email'>Email</label>
+                <input type="email" id='email' name='email' placeholder='Enter email address'/>
+            </div>
+            <div className="input-group">
+                <label htmlFor='password'>Password</label>
+                <input type="password" id='password' name='password' placeholder='Enter password'/>
+            </div>
+
+            <button className='button primary-button'>Register </button>
+
+        </form>
+
+        
+    </div>
+
+    </main>
   )
 }
 
