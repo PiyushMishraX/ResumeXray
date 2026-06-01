8a96fd0ae5f5ee0e94f877601503d467dcc67ad8
Piyushmishrax <piyushmishra.connect@gmail.com>
2026-06-01 22:34:43 +0530

    Use axios to fetch apis and fethc register api

--- Frontend/src/features/auth/services/auth.api.js
+++ Frontend/src/features/auth/services/auth.api.js
@@ -1,4 +1,22 @@
 //  in this file we write fully how frontend interacts with backend
 // we wirte a.b.js instead of .jsx because it distinguise between functionality and react
+// total 4 apis on backend
+
+//  package needed for frontend to communicate with backend or easily communicate wiht backend --> axios
+
+import axios from "axios"
+
+export async function register ({username , email , password}) {
+
+  axios.post("https://localhost:3000/api/auth/register",{
+    username, email, password
+  },{
+    withCredentials: true
+
+    //  axios by default do not give server the access of cookies so we set it using a flag with credentials true
+    //  now server have access that it can access , read or set data on and from cookies
+
+  })
 
 
+}
