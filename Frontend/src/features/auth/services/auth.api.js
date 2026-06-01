7a27056ceb62fa3ee098ae12887b39e3997088b7
Piyushmishrax <piyushmishra.connect@gmail.com>
2026-06-01 22:51:09 +0530

    Get data from the response and fetch login api

--- Frontend/src/features/auth/services/auth.api.js
+++ Frontend/src/features/auth/services/auth.api.js
@@ -2,21 +2,52 @@
 // we wirte a.b.js instead of .jsx because it distinguise between functionality and react
 // total 4 apis on backend
 
+
 //  package needed for frontend to communicate with backend or easily communicate wiht backend --> axios
 
 import axios from "axios"
 
 export async function register ({username , email , password}) {
 
-  axios.post("https://localhost:3000/api/auth/register",{
-    username, email, password
-  },{
-    withCredentials: true
+  try {
 
-    //  axios by default do not give server the access of cookies so we set it using a flag with credentials true
-    //  now server have access that it can access , read or set data on and from cookies
+    const response = await axios.post("https://localhost:3000/api/auth/register",{
+      username, email, password
+    },{
+      withCredentials: true
+      
+      //  axios by default do not give server the access of cookies so we set it using a flag with credentials true
+      //  now server have access that it can access , read or set data on and from cookies
+      
+    })
 
-  })
+    return response.data
+    
+  }catch (err){
+
+    console.log(err)
+
+  }
+
 
 
 }
+
+
+
+export async function login ({ email , password}) {
+
+  try {
+    
+    const response = await axios.post("http://localhost:3000/api/auth/login", {
+      email, password
+    }, { withCredentials: true})
+
+    return response.data
+    
+  }catch (err){
+    console.log(err)
+  }
+
+}
+
