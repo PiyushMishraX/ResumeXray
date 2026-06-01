a189104b3ddc90eea9d959d62d5d0152d77e0650
Piyushmishrax <piyushmishra.connect@gmail.com>
2026-06-01 22:59:15 +0530

    create and use instance of sxios to reduce repeatition

--- Frontend/src/features/auth/services/auth.api.js
+++ Frontend/src/features/auth/services/auth.api.js
@@ -7,18 +7,36 @@
 
 import axios from "axios"
 
+// To remove repeatative code create instance of axios pass repeatetive functionalities and use api.get/post intant of axios.get/post
+
+const api = axios.create({
+  baseURL: "http://localhost:3000",
+  withCredentials: true
+})
+
+
 export async function register ({username , email , password}) {
 
+  // try {
+
+  //   const response = await axios.post("http://localhost:3000/api/auth/register",{
+  //     username, email, password
+  //   },{
+  //     withCredentials: true
+      
+  //     //  axios by default do not give server the access of cookies so we set it using a flag with credentials true
+  //     //  now server have access that it can access , read or set data on and from cookies
+      
+  //   })
+
+  //   return response.data
+    
+  // }catch (err){
+  
   try {
 
-    const response = await axios.post("https://localhost:3000/api/auth/register",{
+    const response = await api.post("/api/auth/register",{
       username, email, password
-    },{
-      withCredentials: true
-      
-      //  axios by default do not give server the access of cookies so we set it using a flag with credentials true
-      //  now server have access that it can access , read or set data on and from cookies
-      
     })
 
     return response.data
@@ -39,9 +57,9 @@ export async function login ({ email , password}) {
 
   try {
     
-    const response = await axios.post("http://localhost:3000/api/auth/login", {
+    const response = await api.post("/api/auth/login", {
       email, password
-    }, { withCredentials: true})
+    })
 
     return response.data
     
@@ -52,11 +70,16 @@ export async function login ({ email , password}) {
 }
 
 export async function logout(){
+  // try {
+  //   const response = await api.get("http://localhost:3000/api/auth/logout", {
+  //     withCredentials: true
+  //   }) // this api do not need anything so no need to give anything 
+  //   return response.data
+  // } catch (err) {
+  //   console.log(err)    
+  // }
   try {
-    const response = await axios.get("http://localhost:3000/api/auth/logout", {
-      withCredentials: true
-    }) // this api do not need anything so no need to give anything 
-    return response.data
+    const response = await api.get("/api/auth/logout")
   } catch (err) {
     console.log(err)    
   }
@@ -64,15 +87,22 @@ export async function logout(){
 
 export async function getMe(){
 
+  // try {
+  //   const response = await axios.get("http://localhost:3000/api/auth/getme", {
+  //     withCredentials: true
+  //   })
+  //   return response.data
+    
+  // } catch (err) {
+  //  console.log(err)
+  // }
+
   try {
-    const response = await axios.get("http://localhost:3000/api/auth/getme", {
-      withCredentials: true
-    })
+    const response = await api.get("/api/auth/getme")
     return response.data
     
   } catch (err) {
-    
+   console.log(err)
   }
-
-  
 }
+
