f95c4ae2029c2ad33b5fc6fc9667b36971b52aa4
Piyushmishrax <piyushmishra.connect@gmail.com>
2026-06-05 23:45:38 +0530

    handle login , regiser, logout states and apis

--- Frontend/src/features/auth/hooks/useAuth.js
+++ Frontend/src/features/auth/hooks/useAuth.js
@@ -11,9 +11,33 @@ export const useAuth = ()=>{
      // and login api calling part is in api layer
      // loading ( ui ) while the api functions are being called // loading screen part is in ui 
      // setting the loading to true is part of hook layer
+     // checkig the auth.api ( api layer) for which apis to manage + backend/backend documentation for the response from apis
     const handleLogin = async ({ email, password}) =>{
         setLoading(true)
         const data = await login({ email, password})
+
+        setUser(data.user)
+        setLoading(false)
     }
+
+    const handleRegister = async ({ username, email, password}) =>{
+        setLoading(true)
+        const data = await register({ username, email, password})
+
+        setUser(data.user)
+        setLoading(false)
+    }
+
+    const handleLogout = async ({ email, password}) =>{
+        setLoading(true)
+        const data = await logout()
+
+        setUser() // logout returns nothing
+        setLoading(false)
+    }
+
+    return { user, laoding ,handleRegister, handleLogin, handleLogout  }
+
+    
 
 }
