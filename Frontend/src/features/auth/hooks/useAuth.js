48f6c27f5b695f5926c140b0c3c8cd3d93b9166f
Piyushmishrax <piyushmishra.connect@gmail.com>
2026-06-06 22:07:51 +0530

    Use try and catch to handle errors and set the loading to flase in each case

--- Frontend/src/features/auth/hooks/useAuth.js
+++ Frontend/src/features/auth/hooks/useAuth.js
@@ -14,26 +14,42 @@ export const useAuth = ()=>{
      // checkig the auth.api ( api layer) for which apis to manage + backend/backend documentation for the response from apis
     const handleLogin = async ({ email, password}) =>{
         setLoading(true)
-        const data = await login({ email, password})
-
-        setUser(data.user)
-        setLoading(false)
+        try {     
+            const data = await login({ email, password})
+    
+            setUser(data.user)
+        } catch (err) {
+            
+        } finally {
+            setLoading(false)
+        }
     }
 
     const handleRegister = async ({ username, email, password}) =>{
         setLoading(true)
-        const data = await register({ username, email, password})
+        try {     
+            const data = await register({ username, email, password})
 
-        setUser(data.user)
-        setLoading(false)
+            setUser(data.user)
+        } catch (err) {
+            
+        } finally {
+            setLoading(false)
+        }
     }
 
     const handleLogout = async ({ email, password}) =>{
         setLoading(true)
-        const data = await logout()
-
-        setUser() // logout returns nothing
-        setLoading(false)
+        try {
+            const data = await logout()
+    
+            setUser() // logout returns nothing
+            
+        } catch (err) {
+            
+        } finally {
+            setLoading(false)
+        }
     }
 
     return { user, loading ,handleRegister, handleLogin, handleLogout  }
