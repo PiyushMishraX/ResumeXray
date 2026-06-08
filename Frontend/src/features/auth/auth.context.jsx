8583c07a9e7bed57f89a64dcba7930dbdd526661
Piyushmishrax <piyushmishra.connect@gmail.com>
2026-06-08 22:37:54 +0530

    Move the getMe api(refresh fix part) to hook layer for better redeability/ better folder structuring of code

--- Frontend/src/features/auth/auth.context.jsx
+++ Frontend/src/features/auth/auth.context.jsx
@@ -8,8 +8,7 @@ export const AuthProvider = ({children}) => {
     const [user, setUser] = useState(null)
     // const [loading, setLoading] = useState(false)
     const [loading, setLoading] = useState(true)
-
-   
+    
     
 
     // we hydrate the user in the production , page relaod ke baad rehydrate karna hota hai use ko ( this is topic for future ) , so by default we setLoading to true
