a67c2f0b1b5e71e841f4c07082629c51d2148c70
Piyushmishrax <piyushmishra.connect@gmail.com>
2026-06-11 00:31:00 +0530

    Create Interview Preport generaing function with prompt

--- Backend/src/services/ai.service.js
+++ Backend/src/services/ai.service.js
@@ -23,6 +23,7 @@ const ai = new GoogleGenAI({
 // this schema is different from the database 
 // the description describes the quesry to ai model so the better it is the better result we will get
 const interviewReportSchema = z.object({
+    matchScore: z.number().description("A socre between 0 and 100 indicating how well the candidate's profile matches the job description"),
     technicalQuestions: z.array(z.object({
         question: z.string().description("The technical question can be asked in the interview"),
         interntion: z.string().description("The Intention behind asking this question"),
@@ -42,13 +43,32 @@ const interviewReportSchema = z.object({
         focus: z.string().description("THe main focus of this day in the preparation plan, e.g. data structure, system design, mock interviews etc"),
         tasks: z.array(z.string()).describe("List of tasks to be done on this day to follow the preparation plan, e.g read a specific book or article, solve a set of prob;ems, watch a videa etc.")
     })).description("A day-wise preparation plan for the candidate to follow in order to prepare for the interview effectively"),
-    title: z.string().description("The title of the job for which the interview in generated"),
+    // title: z.string().description("The title of the job for which the interview in generated"),
 })
 
 async function generateInterviewReport( resume, selfDescription, jobDescription) {
 
+    // creating prompt with all the data
+    const prompt = `Generate an interview report for a candidate with the following details:
+                      Resume: ${resume}
+                      Self Description: ${selfDescription}  
+                      Job Description: ${jobDescription}
+    `
+
+    const response = await ai.models.generateContent({
+        model: "gemini-2.5-flash",
+        // contents: prompt,
+        contents: "",
+        config: {
+            responseMimeType: "application/json", // format details // json format
+            responseJsonSchema: zodToJsonSchema(interviewReportSchema)
+        }
+
+    })
+
+    console.log(JSON.parse(response.text))
 
     
 }
 
-
+module.exports = generateInterviewReport
