b4ea42467f05a5f47f837b474fb68569c6561bb3
Piyushmishrax <piyushmishra.connect@gmail.com>
2026-06-14 16:08:06 +0530

    Create interviewReport and save the interviewReport in interviewReport Model

--- Backend/src/controllers/interview.controller.js
+++ Backend/src/controllers/interview.controller.js
@@ -1,5 +1,6 @@
 const pdfParse = require("pdf-parse") // to get content which is under the file
 const generateInterviewReport = require("../services/ai.service")
+const interviewReportModel = require("../models/interviewReport.model")
 
 async function generateInterviewReportController(req, res) {
     const resumeFile = req.file
@@ -12,6 +13,19 @@ async function generateInterviewReportController(req, res) {
   const interviewReportByAi = await generateInterviewReport({
     resume: resumeContent,
     selfDescription
+  })
+
+  const interviewReport = await interviewReportModel.create({
+    user: req.user.id, // so that is why we pass id in the response in the auth controllers to create connections between in schemas // the id of logged in user
+    resume: resumeContent,
+    selfDescription,
+    jobDescritption,
+    ...interviewReportByAi // all the data/answer provided by ai/destructured
+  })
+
+  res.status(201).json({
+    message: "Interview report generated successfully.",
+    interviewReport,
   })
     
 }
